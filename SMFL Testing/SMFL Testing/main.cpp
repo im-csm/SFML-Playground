2263cbd6c7f3de655c6f23b60fe5a604c1079fe4
Chance Mullen <csmullen8509@eagle.fgcu.edu>
2022-08-09 01:57:04 -0400

    Created a function that moves the ball around and bounces it off the edges of the screen. I tried simulating the old school DVD screensaver. Yes it hits the corner.. eventually.

--- SMFL Testing/SMFL Testing/main.cpp
+++ SMFL Testing/SMFL Testing/main.cpp
@@ -4,18 +4,35 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
+void screenBounce(sf::CircleShape* circle, int* x_Offset, int* y_Offset, sf::RenderWindow* window) {
+    circle->move(*x_Offset, *y_Offset);
+    if (circle->getPosition().x + circle->getLocalBounds().width / 2 >= window->getSize().x)
+        *x_Offset = -*x_Offset;
+    if (circle->getPosition().x - circle->getLocalBounds().width / 2 <= 0)
+        *x_Offset = -*x_Offset;
+    if (circle->getPosition().y + circle->getLocalBounds().height / 2 >= window->getSize().y)
+        *y_Offset = -*y_Offset;
+    if (circle->getPosition().y - circle->getLocalBounds().height / 2 <= 0)
+        *y_Offset = -*y_Offset;
+}
 
 int main()
 {
     int width = 1000, height = 800;
-    sf::RenderWindow window(sf::VideoMode(width, height), "SFML works!");       // Create the SFML window
-    sf::CircleShape shape(100.f);       // Create an SFML circle
+    sf::RenderWindow window(sf::VideoMode(width, height), "SFML works!");   // Create the SFML window
+    window.setFramerateLimit(60);
+    
     // Shape manipulation
-    shape.setOrigin(shape.getLocalBounds().width / 2, shape.getLocalBounds().height / 2);
-    shape.setFillColor(sf::Color::Green);
-    shape.setPosition(width / 2, height / 2);
+    sf::CircleShape circle(25.f);   // Create an SFML circle
+    circle.setOrigin(circle.getLocalBounds().width / 2, circle.getLocalBounds().height / 2);
+    circle.setFillColor(sf::Color::Green);
+    circle.setPosition(100, 100);
+    std::vector<sf::Shape*> trail;
+    int xSpeed = 5, ySpeed = 5;
     //RectangleShape rect()
-
+    
     /* Main loop/Game loop. Needed for every program.
     *  The event loop is also necessary for proper event handling.
        You must clear/draw/display for new frames.                  */ 
@@ -25,18 +42,15 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // check all the window's events that were triggered since the last iteration of the loop
-            sf::Event event;
-            while (window.pollEvent(event))
-            {
-                // "close requested" event: we close the window
-                if (event.type == sf::Event::Closed)
-                    window.close();
-            }
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
         }
-        window.clear(sf::Color::Cyan);
-        window.draw(shape);
+        window.clear(sf::Color::Black);
+        window.draw(circle);
+        screenBounce(&circle, &xSpeed, &ySpeed, &window);
         window.display();
+        printf("xSpeed: %d, ySpeed: %d\n", xSpeed, ySpeed);
     }
     return 0;
 }
